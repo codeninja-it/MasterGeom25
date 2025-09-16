58a01475f2fcd7cc91ae15ae85373eb9f9eacfb4
Daniele Simoncini <simoncini@codeninja.it>
2025-09-16 19:03:02 +0200

    Aggiungere i file di progetto.

---
+++ Programma01/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Programma01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // creo le variabili in un formato numerico a 32bit
+            int a = 5;
+            int b = 5;
+            int c = a * b;
+            float lat = 43.345f;
+            float lon = 11.1123f;
+            float numeroacaso = lat * lon;
+            // scrivo all'utente il risultato
+            System.Console.Write(a);
+            System.Console.Write(" * ");
+            System.Console.Write(b);
+            System.Console.Write(" = ");
+            System.Console.Write(c);
+        }
+    }
+}
