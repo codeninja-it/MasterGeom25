2ee4c8124392a8f56368f469e17c3591d96cdc70
Daniele Simoncini <simoncini@codeninja.it>
2025-09-17 18:15:25 +0200

    fino a programma 3

---
+++ Programma02/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Programma02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Saluto il mio utente, HELO
+            Console.WriteLine("Benvenuto in Tabelline!");
+            // gli pongo la mia domanda
+            Console.Write("Mi dici che tabellina vuoi?");
+            // predispongo una variabile dove conservare le informazioni
+            string risposta = Console.ReadLine();
+            // effettuo il parsing (od il casting) in formato numerico
+            int numero = int.Parse(risposta);
+            // ripeto per sapere quante moltiplicazioni fare
+            Console.Write("E quante volte lo dovrei fare?");
+            // leggo la sua risposta
+            risposta = Console.ReadLine();
+            int ripetizioni = int.Parse(risposta);
+            // imposto un contatore
+            int n = 0;
+            // imposto un ciclo
+            do
+            {
+                int risultato = numero * n;
+                n = n + 1;
+                Console.WriteLine(risultato);
+            } while (n < ripetizioni);
+            // che conclude solo quando la condizione non è più valida
+        }
+    }
+}
---
+++ Programma03/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Programma03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // chiedo quale tabellina
+            Console.Write("Quale tabellina vuoi? ");
+            int numero = int.Parse( Console.ReadLine() );
+            // ne chiedo anche la lunghezza
+            Console.Write("Per quante volte ripeto?");
+            int ripetizioni = int.Parse( Console.ReadLine() );
+            // chiedo dove scaricare i dati
+            Console.Write("Come chiamo il file?");
+            string nomeFile = Console.ReadLine();
+            // inizializzo un buffer vuoto
+            string buffer = "";
+            // eseguo il mio ciclo
+            for (int n = 0; n < ripetizioni; n = n + 1)
+            {
+                int risultato = numero * n;
+                //Console.WriteLine(numero + " X " + n + " = " + risultato);
+                //Console.WriteLine($"{numero} X {n} = {risultato}");
+                buffer = buffer + $"{numero} X {n} = {risultato}\n";
+            }
+            // scarico i dati su disco
+            File.WriteAllText(nomeFile, buffer);
+        }
+    }
+}
